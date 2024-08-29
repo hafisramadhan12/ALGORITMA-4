b7fdac4e79f575de67ff95868e36c706bbab937f
hafidznasution <rhafis709@gmail.com>
2024-08-29 11:33:55 +0700

    print r aray php

---
+++ 10 print r aray php.php
@@ -0,0 +1,20 @@
+<?php
+$array = array(
+    'name' => 'hafis<br>',
+    'age' => 16,
+    '<br>skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+print_r($array);
+
+print "<p>";
+
+$array = array(
+    'name' => 'ilyas<br>',
+    'age' => 45,
+    '<br>skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+var_dump($array);
+
+print "<p>";
