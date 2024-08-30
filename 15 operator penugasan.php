2e541b9be11b7475766aa1478ff13752be9e25a6
hafidznasution <rhafis709@gmail.com>
2024-08-30 08:51:01 +0700

    operator penugasan

---
+++ 15 operator penugasan.php
@@ -0,0 +1,16 @@
+<?php
+$speed = 93;
+
+// ini opertor aritmatika
+$speed = $speed + 10;
+
+// maka nilai speed akan samadengan 83 + 10 = 93
+
+// ini operator penugasan
+$speed += 40;
+
+// sekarang nilai speed akan menjadi 93 + 10 = 103
+
+echo "hasil speed = $speed";
+
+print_r("<p><strong><i>By M HAFIDZ RAMADHAN NST!</strong>");
