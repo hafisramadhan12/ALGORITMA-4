032d49fc32790d78940417141adb137e23bd546a
hafidznasution <rhafis709@gmail.com>
2024-08-30 10:37:15 +0700

    kombinasi dengan elsif

---
+++ 58 kombinasi dengan elseif .php
@@ -0,0 +1,12 @@
+<?php
+$suhu = 30;
+
+if ($suhu < 20) {
+    echo "Cuaca dingin.";
+} elseif ($suhu <= 30) {
+    echo "Cuaca hangat.";
+} else {
+    echo "Cuaca panas.";
+}
+
+print_r("<p><strong><i>By M HAFIDZ RAMADHAN NST!</strong>");
