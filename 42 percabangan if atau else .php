00278abc69222d3716a2ceeb70145a2709775c0e
hafidznasution <rhafis709@gmail.com>
2024-08-30 09:55:21 +0700

    percapangna if atau false

---
+++ 42 percabangan if atau else .php
@@ -0,0 +1,13 @@
+<?php
+
+$umur = 13;
+
+if ($umur < 18 ){
+    echo "Kamu tidak boleh membuka situs ini!";
+} else {
+    echo "Selamat datang di website kami!";
+}
+
+echo "<br><br><br><strong>by M HAFIDZ RAMADHAN NST!";
+
+?>
