23c1bbfeeb79898e3105362c2dc129d5fa1dbf14
hafidznasution <rhafis709@gmail.com>
2024-08-30 10:19:46 +0700

    pernyataan switch

---
+++ 51 peryataan switch .php
@@ -0,0 +1,14 @@
+<?php
+$grade = 'A';
+switch ($grade) {
+  case 'A':
+    echo "Luar biasa!";
+    break;
+  case 'B':
+  case 'C':
+    echo "Bagus!";
+    break;
+  default:
+    echo "Tetap semangat!";
+}
+print_r("<p><strong><i>By M HAFIDZ RAMADHAN NST!</strong>");
