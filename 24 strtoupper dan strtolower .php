32573559797af0d899cae36b938b9806b14c173b
hafidznasution <rhafis709@gmail.com>
2024-08-30 09:12:50 +0700

    strtoupper

---
+++ 24 strtoupper dan strtolower .php
@@ -0,0 +1,5 @@
+<?php
+echo strtoupper("Hello hafis code!"); // Outputs: "hafis code!"
+echo strtolower("Hello World!"); // Outputs: "hello world!"
+
+print_r("<p><strong><i>By M HAFIDZ RAMADHAN NST!</strong>");
