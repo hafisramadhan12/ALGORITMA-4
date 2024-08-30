279d31983ff2b0c2bdbb97ce92188a5053a56cb9
hafidznasution <rhafis709@gmail.com>
2024-08-30 10:17:07 +0700

    pernyataan elsif

---
+++ 50 peryataan elseif .php
@@ -0,0 +1,10 @@
+<?php
+$nilai = 89;
+if ($nilai >= 90) {
+  echo "Nilai kamu A";
+} elseif ($nilai >= 80) {
+  echo "Nilai kamu B";
+} else {
+  echo "Nilai kamu C";
+}
+print_r("<p><strong><i>By M HAFIDZ RAMADHAN NST!</strong>");
